b351ad259f719a22eeeb9865e1f5cd419ec3a6d8
ojhdt <ojhdtmail@gmail.com>
2021-02-05 23:18:53 +0800

    add pulldown refresh

--- pages/history/history.js
+++ pages/history/history.js
@@ -6,24 +6,21 @@ Page({
    */
   data: {
     db:{
-      user1:{
-        task1:{
-          title:"C语言程序设计",
-          admin:"冼广铭",
-          id:"#001",
-          dld:"2021-02-05",
-          dlt:"23:59",
-          finished: false
-        },
-        task2:{
-          title:"数学",
-          admin:"翁文",
-          id:"#002",
-          dld:"2021-02-06",
-          dlt:"00:01",
-          finished: true
-        }
-      }
+      user1:[{
+        title:"C语言程序设计",
+        admin:"冼广铭",
+        id:"#001",
+        dld:"2021-02-05",
+        dlt:"23:59",
+        finished: false
+      },{
+        title:"数学",
+        admin:"翁文",
+        id:"#002",
+        dld:"2021-02-06",
+        dlt:"00:01",
+        finished: true
+      }]
     }
   },
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,5 @@
 //index.js
 Page({
-
   /**
    * 页面的初始数据
    */
@@ -9,24 +8,21 @@ Page({
     finished: null,
     unfinished: null,
     db:{
-      user1:{
-        task1:{
-          title:"C语言程序设计",
-          admin:"冼广铭",
-          id:"#001",
-          dld:"2021-02-05",
-          dlt:"23:59",
-          finished: false
-        },
-        task2:{
-          title:"数学",
-          admin:"翁文",
-          id:"#002",
-          dld:"2021-02-06",
-          dlt:"00:01",
-          finished: true
-        }
-      }
+      user1:[{
+        title:"C语言程序设计",
+        admin:"冼广铭",
+        id:"#001",
+        dld:"2021-02-05",
+        dlt:"23:59",
+        finished: false
+      },{
+        title:"数学",
+        admin:"翁文",
+        id:"#002",
+        dld:"2021-02-06",
+        dlt:"00:01",
+        finished: true
+      }]
     }
   },
 
@@ -36,6 +32,13 @@ Page({
   onLoad: function (options) {
     var time = new Date()
     console.log(time)
+    this.onPullDownRefresh()
+    //设置变量值
+    this.setData({
+      dying: 1,
+      finished: 2,
+      unfinished: 3
+    })
   },
 
   /**
@@ -70,7 +73,24 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    let finished = 0, unfinished = 0;
+    console.log(this.data.db.user1)
+    this.data.db.user1.forEach(element => {
+      if(element.finished == true) finished++;
+      else unfinished++;
+      this.setData({
+        finished:finished,
+        unfinished:unfinished
+      })
+    });
+    wx.stopPullDownRefresh({
+      success: (res) => {
+        wx.showToast({
+          title: "数据已更新",
+          duration: 500
+        })
+      },
+    })
   },
 
   /**
@@ -86,4 +106,5 @@ Page({
   onShareAppMessage: function () {
 
   }
+
 })
