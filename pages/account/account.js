185a3e93f8b51f549404862048fea4663270576e
ojhdt <ojhdtmail@gmail.com>
2021-02-08 21:22:29 +0800

    add login

--- pages/account/account.js
+++ pages/account/account.js
@@ -1,10 +1,13 @@
 // pages/account/account.js
+const app = getApp()
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
+    login: false,
+    avatarUrl: "/source/img/account/avatar.png",
+    nickName: "未登录",
     user:{
       name: "Ojhdt",
       description: "个性签名",
@@ -14,6 +17,45 @@ Page({
     imgUrl: "",
     hitokoto: false,
     bing: true
+  },
+
+  refreshData: function(){
+    if(this.data.login){
+      this.setData({
+        nickName: this.data.userInfo.nickName,
+        avatarUrl: this.data.userInfo.avatarUrl
+      })
+    }
+    else{console.log("failed")}
+  },
+
+  refreshUserInfo: function(){
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        login: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          login: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            login: true
+          })
+        }
+      })
+    }
   },
 
   getImage: function(){
@@ -112,9 +154,11 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this.refreshUserInfo();
     this.getImage();
     this.getMoto();
     this.animation();
+    this.refreshData();
   },
 
   /**
@@ -128,7 +172,8 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    this.refreshUserInfo()
+    this.refreshData()
   },
 
   /**
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,11 +1,13 @@
 //index.js
+const app = getApp()
 var util = require("../../utils/util.js")
 Page({
   /**
    * 页面的初始数据
    */
   data: {
-    login: true,
+    login: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
     db:{
       user1:[{
         title: "C语言程序设计",
@@ -36,50 +38,70 @@ Page({
     }
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    if(this.data.login)
-    this.onPullDownRefresh()
-    //设置变量值
-    this.setData({
-
-    })
+  getUserInfo: function(e){
+    app.globalData.userInfo = e.detail.userInfo;
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        login: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          login: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            login: true
+          })
+        }
+      })
+    }
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  refreshUserInfo: function(){
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        login: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          login: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            login: true
+          })
+        }
+      })
+    }
+  },
+
+  loading: function(){
 
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+  sortData: function(){
     // var time = util.formatTime(new Date());
     // console.log((new Date).getTime());
     let finished = 0, unfinished = 0, dying = 0;
@@ -224,6 +246,52 @@ Page({
       unfinished:unfinished,
       dying: dying
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    this.refreshUserInfo()
+    //已授权自动更新
+    if(this.data.login)
+    this.sortData()
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    this.refreshUserInfo()
+    this.sortData()
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    this.sortData()
     wx.stopPullDownRefresh({
       success: (res) => {
         wx.showToast({
